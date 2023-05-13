349d131a5df24600e327c29e2890cddf5f61b69e
Radouane OUBAKHANE <radouane.oubakhane@usms.ac.ma>
2023-05-13 19:45:18 +0100

    Update service inscription

--- service-inscription/src/main/java/com/radouaneoubakhane/serviceinscription/service/InscriptionService.java
+++ service-inscription/src/main/java/com/radouaneoubakhane/serviceinscription/service/InscriptionService.java
@@ -1,17 +1,12 @@
 package com.radouaneoubakhane.serviceinscription.service;
 
 
-import com.radouaneoubakhane.serviceinscription.dto.EtudientRequest;
-import com.radouaneoubakhane.serviceinscription.dto.FilierResponse;
-import com.radouaneoubakhane.serviceinscription.dto.InscriptionRequest;
-import com.radouaneoubakhane.serviceinscription.dto.InscriptionResponse;
-import com.radouaneoubakhane.serviceinscription.exception.FiliereNotFoundException;
+import com.radouaneoubakhane.serviceinscription.dto.*;
 import com.radouaneoubakhane.serviceinscription.exception.InscriptionAlreadyAcceptedOrRefusedOrCanceledException;
 import com.radouaneoubakhane.serviceinscription.exception.InscriptionNotFoundException;
 import com.radouaneoubakhane.serviceinscription.model.Inscription;
 import com.radouaneoubakhane.serviceinscription.model.enums.Diplomat;
 import com.radouaneoubakhane.serviceinscription.openfeingClients.EtudientClient;
-import com.radouaneoubakhane.serviceinscription.openfeingClients.FiliereClient;
 import com.radouaneoubakhane.serviceinscription.repository.InscriptionRepository;
 import com.radouaneoubakhane.serviceinscription.validations.ObjectsValidator;
 import jakarta.transaction.Transactional;
@@ -30,14 +25,18 @@ import java.util.UUID;
 public class InscriptionService {
 
     private final InscriptionRepository inscriptionRepository;
-    private final FiliereClient filiereClient;
     private final EtudientClient etudientClient;
     private final ObjectsValidator<InscriptionRequest> objectsValidator;
+    private final ObjectsValidator<InscriptionRequestLST> inscriptionRequestLSTObjectsValidator;
+    private final ObjectsValidator<InscriptionRequestDEUST> inscriptionObjectsValidatorDEUST;
 
 
-    public List<InscriptionResponse> getAllInscriptions() {
+    public List<InscriptionResponse> getAllInscriptionsMST() {
         log.info("Getting all inscriptions");
-        List<Inscription> inscriptions = inscriptionRepository.findAll();
+        List<Inscription> inscriptions = inscriptionRepository
+                .findAllByDiplomat(Diplomat.MST).stream()
+                .filter(inscription -> !inscription.isCanceled() && !inscription.isAccepted() && !inscription.isRefused())
+                .toList();
         return inscriptions.stream()
                 .map(this::mapInscriptionToInscriptionResponse)
                 .toList();
@@ -52,10 +51,6 @@ public class InscriptionService {
 
     public InscriptionResponse createInscription(InscriptionRequest inscriptionRequest) {
         objectsValidator.validate(inscriptionRequest);
-        FilierResponse filier = filiereClient.getFilierById(inscriptionRequest.getFiliereId());
-        if (filier == null) {
-            throw new FiliereNotFoundException("Filiere not found");
-        }
         
         Inscription inscription = mapInscriptionRequestToInscription(inscriptionRequest);
 
@@ -73,10 +68,6 @@ public class InscriptionService {
         Inscription inscription = inscriptionRepository.findById(inscriptionId)
                 .orElseThrow(() -> new InscriptionNotFoundException("Inscription not found"));
 
-        FilierResponse filier = filiereClient.getFilierById(inscriptionRequest.getFiliereId());
-        if (filier == null) {
-            throw new FiliereNotFoundException("Filiere not found");
-        }
 
         inscription.setCin(inscriptionRequest.getCin());
         inscription.setCne(inscriptionRequest.getCne());
@@ -201,7 +192,7 @@ public class InscriptionService {
         List<Inscription> inscriptions = inscriptionRepository
                 .findAllByDiplomat(Diplomat.DEUST).stream()
                 .filter(inscription -> !inscription.isCanceled())
-                .toList();;
+                .toList();
 
 
         List<EtudientRequest> etudientRequests = inscriptions.stream()
@@ -224,7 +215,7 @@ public class InscriptionService {
         List<Inscription> inscriptions = inscriptionRepository
                 .findAllByDiplomat(Diplomat.LST).stream()
                 .filter(inscription -> !inscription.isCanceled())
-                .toList();;
+                .toList();
 
         List<EtudientRequest> etudientRequests = inscriptions.stream()
                 .sorted(Comparator.comparing(Inscription::getMoyenneDiplome).reversed())
@@ -347,4 +338,221 @@ return InscriptionResponse.builder()
                 .build();
     }
 
+    public List<InscriptionResponseDEUST> getAllInscriptionsDEUST() {
+        return inscriptionRepository.findAllByDiplomat(Diplomat.DEUST).stream()
+                .filter(inscription -> !inscription.isCanceled() && !inscription.isRefused() && !inscription.isAccepted())
+                .map(this::mapInscriptionToInscriptionResponseDEUST)
+                .toList();
+    }
+
+    private InscriptionResponseDEUST mapInscriptionToInscriptionResponseDEUST(Inscription inscription) {
+        return InscriptionResponseDEUST.builder()
+                .id(inscription.getId())
+                .cin(inscription.getCin())
+                .cne(inscription.getCne())
+                .nom(inscription.getNom())
+                .prenom(inscription.getPrenom())
+                .genre(inscription.getGenre())
+                .email(inscription.getEmail())
+                .dateNaissance(inscription.getDateNaissance())
+                .lieuNaissance(inscription.getLieuNaissance())
+                .adresse(inscription.getAdresse())
+                .telephone(inscription.getTelephone())
+                .filiereBac(inscription.getFiliereBac())
+                .moyenneBac(inscription.getMoyenneBac())
+                .mentionBac(inscription.getMentionBac())
+                .anneeBac(inscription.getAnneeBac())
+                .etablissementBac(inscription.getEtablissementBac())
+                .villeBac(inscription.getVilleBac())
+                .paysBac(inscription.getPaysBac())
+                .filiereId(inscription.getFiliereId())
+                .diplomat(inscription.getDiplomat())
+                .build();
+    }
+
+    public List<InscriptionResponseLST> getAllInscriptionsLST() {
+        return inscriptionRepository.findAllByDiplomat(Diplomat.LST).stream()
+                .filter(inscription -> !inscription.isCanceled() && !inscription.isRefused() && !inscription.isAccepted())
+                .map(this::mapInscriptionToInscriptionResponseLST)
+                .toList();
+    }
+
+    private InscriptionResponseLST mapInscriptionToInscriptionResponseLST(Inscription inscription) {
+        return InscriptionResponseLST.builder()
+                .id(inscription.getId())
+                .cin(inscription.getCin())
+                .cne(inscription.getCne())
+                .nom(inscription.getNom())
+                .prenom(inscription.getPrenom())
+                .genre(inscription.getGenre())
+                .email(inscription.getEmail())
+                .dateNaissance(inscription.getDateNaissance())
+                .lieuNaissance(inscription.getLieuNaissance())
+                .adresse(inscription.getAdresse())
+                .telephone(inscription.getTelephone())
+                .filiereBac(inscription.getFiliereBac())
+                .moyenneBac(inscription.getMoyenneBac())
+                .mentionBac(inscription.getMentionBac())
+                .anneeBac(inscription.getAnneeBac())
+                .etablissementBac(inscription.getEtablissementBac())
+                .villeBac(inscription.getVilleBac())
+                .paysBac(inscription.getPaysBac())
+                .diplomeDeust(inscription.getDiplomeDeust())
+                .moyenneDiplome(inscription.getMoyenneDiplome())
+                .mentionDiplome(inscription.getMentionDiplome())
+                .anneeDiplome(inscription.getAnneeDiplome())
+                .etablissementDiplome(inscription.getEtablissementDiplome())
+                .villeDiplome(inscription.getVilleDiplome())
+                .paysDiplome(inscription.getPaysDiplome())
+                .filiereId(inscription.getFiliereId())
+                .diplomat(inscription.getDiplomat())
+                .build();
+    }
+
+    public InscriptionResponseDEUST createInscriptionDEUST(InscriptionRequestDEUST inscriptionRequestDEUST) {
+        inscriptionObjectsValidatorDEUST.validate(inscriptionRequestDEUST);
+        Inscription inscription = mapInscriptionRequestDEUSTToInscription(inscriptionRequestDEUST);
+        inscription.setDiplomat(Diplomat.DEUST);
+        inscription.setCanceled(false);
+        inscription.setAccepted(false);
+        inscription.setRefused(false);
+        inscriptionRepository.save(inscription);
+        return mapInscriptionToInscriptionResponseDEUST(inscription);
+    }
+
+    private Inscription mapInscriptionRequestDEUSTToInscription(InscriptionRequestDEUST inscriptionRequestDEUST) {
+        return Inscription.builder()
+                .cin(inscriptionRequestDEUST.getCin())
+                .cne(inscriptionRequestDEUST.getCne())
+                .nom(inscriptionRequestDEUST.getNom())
+                .prenom(inscriptionRequestDEUST.getPrenom())
+                .email(inscriptionRequestDEUST.getEmail())
+                .genre(inscriptionRequestDEUST.getGenre())
+                .dateNaissance(inscriptionRequestDEUST.getDateNaissance())
+                .lieuNaissance(inscriptionRequestDEUST.getLieuNaissance())
+                .adresse(inscriptionRequestDEUST.getAdresse())
+                .telephone(inscriptionRequestDEUST.getTelephone())
+                .filiereBac(inscriptionRequestDEUST.getFiliereBac())
+                .moyenneBac(inscriptionRequestDEUST.getMoyenneBac())
+                .mentionBac(inscriptionRequestDEUST.getMentionBac())
+                .anneeBac(inscriptionRequestDEUST.getAnneeBac())
+                .etablissementBac(inscriptionRequestDEUST.getEtablissementBac())
+                .villeBac(inscriptionRequestDEUST.getVilleBac())
+                .paysBac(inscriptionRequestDEUST.getPaysBac())
+                .filiereId(inscriptionRequestDEUST.getFiliereId())
+                .diplomat(inscriptionRequestDEUST.getDiplomat())
+                .build();
+    }
+
+    public InscriptionResponseLST createInscriptionLST(InscriptionRequestLST inscriptionRequestLST) {
+        inscriptionRequestLSTObjectsValidator.validate(inscriptionRequestLST);
+        Inscription inscription = mapInscriptionRequestToInscriptionLST(inscriptionRequestLST);
+        inscription.setDiplomat(Diplomat.LST);
+        inscription.setCanceled(false);
+        inscription.setAccepted(false);
+        inscription.setRefused(false);
+        inscriptionRepository.save(inscription);
+        return mapInscriptionToInscriptionResponseLST(inscription);
+    }
+
+    private Inscription mapInscriptionRequestToInscriptionLST(InscriptionRequestLST inscriptionRequestLST) {
+        return Inscription.builder()
+                .cin(inscriptionRequestLST.getCin())
+                .cne(inscriptionRequestLST.getCne())
+                .nom(inscriptionRequestLST.getNom())
+                .prenom(inscriptionRequestLST.getPrenom())
+                .email(inscriptionRequestLST.getEmail())
+                .genre(inscriptionRequestLST.getGenre())
+                .dateNaissance(inscriptionRequestLST.getDateNaissance())
+                .lieuNaissance(inscriptionRequestLST.getLieuNaissance())
+                .adresse(inscriptionRequestLST.getAdresse())
+                .telephone(inscriptionRequestLST.getTelephone())
+                .filiereBac(inscriptionRequestLST.getFiliereBac())
+                .moyenneBac(inscriptionRequestLST.getMoyenneBac())
+                .mentionBac(inscriptionRequestLST.getMentionBac())
+                .anneeBac(inscriptionRequestLST.getAnneeBac())
+                .etablissementBac(inscriptionRequestLST.getEtablissementBac())
+                .villeBac(inscriptionRequestLST.getVilleBac())
+                .paysBac(inscriptionRequestLST.getPaysBac())
+                .diplomeDeust(inscriptionRequestLST.getDiplomeDeust())
+                .moyenneDiplome(inscriptionRequestLST.getMoyenneDiplome())
+                .mentionDiplome(inscriptionRequestLST.getMentionDiplome())
+                .anneeDiplome(inscriptionRequestLST.getAnneeDiplome())
+                .etablissementDiplome(inscriptionRequestLST.getEtablissementDiplome())
+                .villeDiplome(inscriptionRequestLST.getVilleDiplome())
+                .paysDiplome(inscriptionRequestLST.getPaysDiplome())
+                .filiereId(inscriptionRequestLST.getFiliereId())
+                .diplomat(inscriptionRequestLST.getDiplomat())
+                .build();
+    }
+
+    public InscriptionResponseDEUST updateInscriptionDEUST(Long inscriptionId, InscriptionRequestDEUST inscriptionRequestDEUST) {
+        inscriptionObjectsValidatorDEUST.validate(inscriptionRequestDEUST);
+        log.info("Updating inscription with id {}", inscriptionId);
+        Inscription inscription = inscriptionRepository.findById(inscriptionId)
+                .orElseThrow(() -> new InscriptionNotFoundException("Inscription not found"));
+
+
+        inscription.setCin(inscriptionRequestDEUST.getCin());
+        inscription.setCne(inscriptionRequestDEUST.getCne());
+        inscription.setNom(inscriptionRequestDEUST.getNom());
+        inscription.setPrenom(inscriptionRequestDEUST.getPrenom());
+        inscription.setEmail(inscriptionRequestDEUST.getEmail());
+        inscription.setDateNaissance(inscriptionRequestDEUST.getDateNaissance());
+        inscription.setLieuNaissance(inscriptionRequestDEUST.getLieuNaissance());
+        inscription.setAdresse(inscriptionRequestDEUST.getAdresse());
+        inscription.setTelephone(inscriptionRequestDEUST.getTelephone());
+        inscription.setFiliereBac(inscriptionRequestDEUST.getFiliereBac());
+        inscription.setMoyenneBac(inscriptionRequestDEUST.getMoyenneBac());
+        inscription.setMentionBac(inscriptionRequestDEUST.getMentionBac());
+        inscription.setAnneeBac(inscriptionRequestDEUST.getAnneeBac());
+        inscription.setEtablissementBac(inscriptionRequestDEUST.getEtablissementBac());
+        inscription.setVilleBac(inscriptionRequestDEUST.getVilleBac());
+        inscription.setPaysBac(inscriptionRequestDEUST.getPaysBac());
+        inscription.setDiplomat(inscriptionRequestDEUST.getDiplomat());
+        inscription.setFiliereId(inscriptionRequestDEUST.getFiliereId());
+
+        Inscription savedInscription = inscriptionRepository.save(inscription);
+
+        return mapInscriptionToInscriptionResponseDEUST(savedInscription);
+    }
+
+    public InscriptionResponseLST updateInscriptionLST(Long inscriptionId, InscriptionRequestLST inscriptionRequestLST) {
+        inscriptionRequestLSTObjectsValidator.validate(inscriptionRequestLST);
+        log.info("Updating inscription with id {}", inscriptionId);
+        Inscription inscription = inscriptionRepository.findById(inscriptionId)
+                .orElseThrow(() -> new InscriptionNotFoundException("Inscription not found"));
+
+
+        inscription.setCin(inscriptionRequestLST.getCin());
+        inscription.setCne(inscriptionRequestLST.getCne());
+        inscription.setNom(inscriptionRequestLST.getNom());
+        inscription.setPrenom(inscriptionRequestLST.getPrenom());
+        inscription.setEmail(inscriptionRequestLST.getEmail());
+        inscription.setDateNaissance(inscriptionRequestLST.getDateNaissance());
+        inscription.setLieuNaissance(inscriptionRequestLST.getLieuNaissance());
+        inscription.setAdresse(inscriptionRequestLST.getAdresse());
+        inscription.setTelephone(inscriptionRequestLST.getTelephone());
+        inscription.setFiliereBac(inscriptionRequestLST.getFiliereBac());
+        inscription.setMoyenneBac(inscriptionRequestLST.getMoyenneBac());
+        inscription.setMentionBac(inscriptionRequestLST.getMentionBac());
+        inscription.setAnneeBac(inscriptionRequestLST.getAnneeBac());
+        inscription.setEtablissementBac(inscriptionRequestLST.getEtablissementBac());
+        inscription.setVilleBac(inscriptionRequestLST.getVilleBac());
+        inscription.setPaysBac(inscriptionRequestLST.getPaysBac());
+        inscription.setDiplomeDeust(inscriptionRequestLST.getDiplomeDeust());
+        inscription.setMoyenneDiplome(inscriptionRequestLST.getMoyenneDiplome());
+        inscription.setMentionDiplome(inscriptionRequestLST.getMentionDiplome());
+        inscription.setAnneeDiplome(inscriptionRequestLST.getAnneeDiplome());
+        inscription.setEtablissementDiplome(inscriptionRequestLST.getEtablissementDiplome());
+        inscription.setVilleDiplome(inscriptionRequestLST.getVilleDiplome());
+        inscription.setPaysDiplome(inscriptionRequestLST.getPaysDiplome());
+        inscription.setDiplomat(inscriptionRequestLST.getDiplomat());
+        inscription.setFiliereId(inscriptionRequestLST.getFiliereId());
+
+        Inscription savedInscription = inscriptionRepository.save(inscription);
+
+        return mapInscriptionToInscriptionResponseLST(savedInscription);
+    }
 }
+
